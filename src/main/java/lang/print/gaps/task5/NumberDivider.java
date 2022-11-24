9dc24efd30a160d6655f721e5eb4c4d887fa3c6e
Stas <chernenkosa@gmail.com>
2022-11-24 03:01:13 +0200

    solution

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double divider = 5.0;
+        double result = toBeDivided/divider;
+        System.out.println(result);
     }
 }
